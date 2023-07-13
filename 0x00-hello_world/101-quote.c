f1ef817cd995ee80c027f7a544ec9efbbd130a5c
Ramyrashad127 <ramy41599@gmail.com>
2023-07-13 13:06:20 -0700

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
     char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
