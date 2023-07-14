1d89b042a3d290b19d09470488306b722bf5cc99
Ramyrashad127 <ramy41599@gmail.com>
2023-07-14 06:25:09 -0700

    new

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
-	char a;
+	int a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		putchar(a);
-		if (a != '9')
+		if (a != 9)
 		putchar(',');
 	}
 	putchar('\n');
