3187fd80d0faa5e36ae2808263668fb914d1ecaf
Ramyrashad127 <ramy41599@gmail.com>
2023-07-14 05:40:21 -0700

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		if(i == 'q' || i == 'e')
+		if (i == 'q' || i == 'e')
 		{
 			continue;
 		}
@@ -19,5 +19,6 @@ int main(void)
 	}
 
 	putchar('\n');
-	return 0;
+
+	return (0);
 }	
