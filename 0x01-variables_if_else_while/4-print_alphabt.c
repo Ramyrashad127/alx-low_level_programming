97450037a3565720677d3a7cb3d20189e7d00192
Ramyrashad127 <ramy41599@gmail.com>
2023-07-14 05:30:46 -0700

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 
 /**
-* main - print alpghabet
-*
-*Return: Always (success)
-*/
+ * main - print alpghbet
+ *
+ * Return: Always (access)
+ */
 int main(void)
 {
-	for (char i = 'a'; i <= 'z'; i++)
+
+	for(char i = 'a'; i <= 'z'; i++)
 	{
-		if (i == 'q' || i == 'e')
+		if(i == 'q' || i == 'e')
 			continue;
 		putchar(i);
 	}
 
 	putchar('\n');
 	return 0;
-	
-}
+}	
