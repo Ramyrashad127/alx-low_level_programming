b2f44c2b92bad3a982fccd0780a4fc9fdbf631d1
Ramyrashad127 <ramy41599@gmail.com>
2023-07-14 05:53:33 -0700

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+* main - print digits
+*
+*Return: Always (success)
+*/
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+    putchar('\n');
+	return 0;
+	
+}
