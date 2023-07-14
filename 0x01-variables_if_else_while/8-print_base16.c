f884f15ba7ba0587e5ceffc8fe8054fea07a7233
Ramyrashad127 <ramy41599@gmail.com>
2023-07-14 06:17:12 -0700

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main - print digits
+*
+* Return: Always (success)
+*/
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
