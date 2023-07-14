c235ad69254aa2df55e73727a84f0cfc782b60f1
Ramyrashad127 <ramy41599@gmail.com>
2023-07-14 05:01:50 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main - print alpghabet
+*
+*Return: Always (success)
+*/
+int main(void)
+{
+	char a = 'a';
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return 0;
+	
+}
