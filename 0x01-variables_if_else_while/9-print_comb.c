b310d2d47f37ad365fa284671b09285a29b1bccf
Ramyrashad127 <ramy41599@gmail.com>
2023-07-14 06:21:10 -0700

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main - print digits
+*
+* Return: Always (success)
+*/
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a != '9');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
