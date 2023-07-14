362a38c0393ac1314949ed5ddd535b3703a6f363
Ramyrashad127 <ramy41599@gmail.com>
2023-07-14 06:46:57 -0700

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,15 @@
 */
 int main(void)
 {
-	int a,b;
+	int a, b;
 
-	for (a = 0,b=0; b <= 9; a++)
+	for (a = 1, b = 0; b <= 9; a++)
 	{
-		putchar(a + 48);
+		if (a == b|| a == 0)
+			continue;
 		putchar(b + 48);
-		if (a == 9 && b == 9)
+		putchar(a + 48);
+		if (a == 9 && b == 8)
 		{
 			break;
 		}
@@ -24,8 +26,8 @@ int main(void)
 		}
 		if (a == 9)
 		{
+			a = b;
 			b++;
-			a = 0;
 		}
 	}
 	putchar('\n');
