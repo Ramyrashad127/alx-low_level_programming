02ac0c16412548c115e5c603d0bc5c9c242ce26e
Ramyrashad127 <ramy41599@gmail.com>
2023-07-18 13:53:30 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include"main"
+
+/**
+ * print_alphabet_x10 - print
+ *
+ * Return:null
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int y=1;
+
+	while(y<=10)
+	{
+		x='a';
+		while(x<=z)
+		{
+			_putchar(x);
+			x++;
+		}
+		putchar('\n');
+	}
+}
