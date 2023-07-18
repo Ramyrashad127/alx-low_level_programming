39853bca3b44515da60090865b7a4f9a8683ad0e
Ramyrashad127 <ramy41599@gmail.com>
2023-07-18 13:35:01 -0700

    new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<main.h>
+
+/**
+* print_alphabet - print 
+*
+* Return: null
+*/
+void print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+	}
+	_putchar("\n");
+}
