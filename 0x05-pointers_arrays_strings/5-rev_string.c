5ad4213b74b001bd2feb9a85813ab07db7062981
Ramyrashad127 <ramy41599@gmail.com>
2023-07-24 03:11:45 -0700

    new

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+/**
+ * rev_string - reverse
+ * @s: string
+ * Return: null
+ */
+void rev_string(char *s)
+{
+	char *s2, count = 0, i, c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s2[i] = s[i];
+		count++;
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		s[c] = s2[i];
+		c++;
+	}
+}
