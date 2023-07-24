0d6fdc321b0670233acfa12d0fa8cbddf14d93a2
Ramyrashad127 <ramy41599@gmail.com>
2023-07-24 03:43:50 -0700

    new

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,16 @@
  */
 void rev_string(char *s)
 {
-  char ch, *p, *q;
+	 char ch, *p, *q;
 
-  for (q = s; *q != '\0'; ++q)
-    ;
-  if (q > s)
-    --q;
-  for (p = s; p < q; ++p, --q) {
-    ch = *p;
-    *p = *q;
-    *q = ch;
-  }
+	for (q = s; *q != '\0'; ++q)
+		;
+	if (q > s)
+		--q;
+	for (p = s; p < q; ++p, --q)
+	{
+		ch = *p;
+		*p = *q;
+		*q = ch;
+	}
 }
