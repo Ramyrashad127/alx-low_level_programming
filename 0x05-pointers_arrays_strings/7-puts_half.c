5e62e6c3a92f1a8af1bf36333ff08682fc8131cd
Ramyrashad127 <ramy41599@gmail.com>
2023-07-24 04:11:42 -0700

    new

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts_half - print
+ * @str: string
+ * Return: null
+ */
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	count /= 2;
+	for (i = 0; i < count; i++)
+		_putchar(str[i]);
+}
