af437cd6c85435ef9c3cf38dda4c2ec33c3ecd28
Ramyrashad127 <ramy41599@gmail.com>
2023-07-24 04:19:17 -0700

    new

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@ void puts_half(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	count /= 2;
-	for (i = 0; i < count; i++)
+	for (i = count; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
