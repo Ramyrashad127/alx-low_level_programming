5340b06bb34afbd0f76708d956c9bcacf08e61f2
Ramyrashad127 <ramy41599@gmail.com>
2023-07-24 02:36:34 -0700

    new

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - len
+ * @s: array
+ * RETURN: int
+ */
+int _strlen(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++ )
+		count++;
+	return (count);
+}
