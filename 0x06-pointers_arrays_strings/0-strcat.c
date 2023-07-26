3b0b52a30a18837808594a09f56f0a2a37c1780d
Ramyrashad127 <ramy41599@gmail.com>
2023-07-26 10:03:38 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _strcat - cat
- * @dest:test
- * @src:test
- * Return:pointer
+ * @dest: test
+ * @src: test
+ * Return: string
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, c;
 
-	for (i = 0; dest[i] != '\0'; i++ )
+	for (i = 0; dest[i] != '\0'; i++)
 		;
 	for (c = 0; src[c] != '\0'; c++)
 	{
