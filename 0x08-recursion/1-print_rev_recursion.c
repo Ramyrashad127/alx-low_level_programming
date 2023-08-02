8a911fa855e108a24c377317608601e63fc4c7f7
Ramyrashad127 <ramy41599@gmail.com>
2023-08-02 13:51:23 -0700

    new

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * _print_rev_recursion - print
  * @s:string
  * Return: pointer
@@ -9,6 +8,6 @@ void _print_rev_recursion(char *s)
 {
 	if (s[0] == '\0')
 		return;
-	_print_rev_recursion(s++);
+	_print_rev_recursion(++s);
 	_putchar(s[0]);
 }
