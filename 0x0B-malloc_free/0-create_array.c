4e6fb1ff46347aaaf26871a9d3a48f7a9fd092c1
Ramyrashad127 <ramy41599@gmail.com>
2023-08-08 11:08:01 -0700

    new

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(size * sizeof(char));
-	if(ptr == NULL)
+	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
