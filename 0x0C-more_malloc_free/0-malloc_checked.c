01057ed6fb237775f1cf86cb168fdbcf85ff13ba
Ramyrashad127 <ramy41599@gmail.com>
2023-08-10 06:21:03 -0700

    new

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 #include <limits.h>
 /**
- * *malloc_checked - check
- * @b:test
- * Return:pointer
+ * *malloc_checked - allocate memory with malloc
+ * @b: unsigned int type
+ * Return: return pointer
  */
 void *malloc_checked(unsigned int b)
 {
-	int *ptr = malloc(b);
+	int *ptr;
 
+	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
 	return (ptr);
