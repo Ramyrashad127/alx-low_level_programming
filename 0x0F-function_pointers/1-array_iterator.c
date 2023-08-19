e10c9b20ad739dca0ac7837a9e04e0e548907afb
Ramyrashad127 <ramy41599@gmail.com>
2023-08-19 02:04:49 -0700

    new

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include"function_pointers.h"
+
 /**
  * array_iterator - test
  * @array: test
@@ -8,11 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
 	if (action)
 	{
 		for (i = 0; i < size; i++)
-		action(array[i]);
+			action(array[i]);
 	}
 }
