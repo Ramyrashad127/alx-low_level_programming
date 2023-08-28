2960e8d04fd1ef8c83be9996f9b863e84a568478
Ramyrashad127 <ramy41599@gmail.com>
2023-08-28 11:30:31 -0700

    new

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
+	const listint_t *ptr = h;
 
-	while (h != NULL)
+	while (ptr != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", ptr->n);
 		num = num + 1;
-		h = h->next;
+		ptr = ptr->next;
 	}
 	return (num);
 }
