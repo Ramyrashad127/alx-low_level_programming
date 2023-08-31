e0e107580b2000e883eaf73682483a725e474fbf
Ramyrashad127 <ramy41599@gmail.com>
2023-08-31 12:05:01 -0700

    new

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - s
+ * @n: n
+ * @index: index
+ * Return: 1   -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int c;
+
+	if (sizeof(*n) * 8 - 1 < index)
+		return (-1);
+	c = 1 << index;
+	c = !c;
+	*n = c & *n;
+	return (1);
+}
