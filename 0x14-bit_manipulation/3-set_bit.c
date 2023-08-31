1fd8af91c24edbd21009edbd42e75dc16eb8ede1
Ramyrashad127 <ramy41599@gmail.com>
2023-08-31 11:53:15 -0700

    new

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_bit - s
+ * @n: n
+ * @index: index
+ * Return: 1   -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (sizeof(n) * 8 - 1 < index)
+		return (-1);
+	return (1 << index | *n);
+}
