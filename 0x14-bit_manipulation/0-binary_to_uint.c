05c1c816197fa307081fbfd179b8db49f8e113bd
Ramyrashad127 <ramy41599@gmail.com>
2023-08-31 11:09:04 -0700

    new

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,16 +7,27 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum = 0,i,c = 0;
-	
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	i--;
-	for (i; i >= 0; i--)
+	unsigned int sum = 0, res = 1;
+	int i, j, c = 0, p;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+		j++;
+	i = j - 1;
+	while (j--)
 	{
-		if (s[i] == '1')
-			sum += pow(2, c);
+		if (b[i] == '1')
+		{
+			for (p = 1; p <= c; p++)
+				res *= 2;
+			sum += res;
+			res = 1;
+		}
+		else if (b[i] != '0')
+			return (0);
 		c++;
+		i--;
 	}
 	return (sum);
 }
