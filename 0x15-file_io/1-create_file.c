8023599fbd81622baa05329ce38a05365026124c
Ramyrashad127 <ramy41599@gmail.com>
2023-09-04 09:23:46 -0700

    new

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - create
+ * @filename: name
+ * text_content
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fn, str = 0, i;
+
+	if (!filename)
+		return (-1);
+	fn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fn == 0)
+		return (-1);
+	if (!text_content)
+	{
+		while (text_content[str] != '\0')
+			str++;
+		i = write(fn, text_content, str);
+		if (i != str)
+			return (-1);
+	}
+	close(fn);
+	return (1);
+}
