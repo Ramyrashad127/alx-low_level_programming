56ef4440b50ae420e8768f73728a490d139a1fd6
Ramyrashad127 <ramy41599@gmail.com>
2023-09-04 11:24:06 -0700

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  * create_file - create
  * @filename: name
- * text_content
+ * text_content: con
  * Return: int
  */
 int create_file(const char *filename, char *text_content)
@@ -22,4 +22,3 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	return (1);
 }
-
