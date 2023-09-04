e014509c35a94d96a43d39e14c873127fd7e5b84
Ramyrashad127 <ramy41599@gmail.com>
2023-09-04 11:37:23 -0700

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
  * create_file - create
- * @filename: name
- * text_content: con
- * Return: int
+ * @filename:name
+ * text_content:con
+ * Return:1 or -1
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fn;
 	ssize_t str = 0, i = 0;
 
+	while (text_content[str] != '\0')
+		str++;
 	if (!filename)
 		return (-1);
 	fn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fn == -1)
 		return (-1);
-	while (text_content[str] != '\0')
-		str++;
 	if (str != 0)
 		i = write(fn, text_content, str);
 	close(fn);
