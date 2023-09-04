72e7e75b53925177dbc18a471ac689396b28e159
Ramyrashad127 <ramy41599@gmail.com>
2023-09-04 11:08:52 -0700

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[str] != '\0')
 		str++;
+	str--;
 	i = write(fn, text_content, str);
 	if (i != str)
 		return (-1);
