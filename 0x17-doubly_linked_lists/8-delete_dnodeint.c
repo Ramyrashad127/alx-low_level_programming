aecd37b7e1aa5823a1aee4bc4dbddaaed0732896
Ramyrashad127 <ramy41599@gmail.com>
2023-10-12 12:38:27 -0700

    new

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - d
+ * @head: head
+ * @idx: index
+ * Return: node
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t test = *head;
+	unsigned int i =0;
+
+	while(test)
+	{
+		if (i == idx)
+			break;
+		test = test->next;
+		i++;
+	}
+	if (i != idx)
+		return (-1);
+	test->prev = test->next;
+	free(test);
+}
