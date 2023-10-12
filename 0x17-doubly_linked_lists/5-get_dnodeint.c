b94f1788fe88456ea900e8fab1b9f80a10b0873e
Ramyrashad127 <ramy41599@gmail.com>
2023-10-12 11:17:50 -0700

    new

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index get
+ * @head: head
+ * @index: index
+ * Return: node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			break;
+		head = head->next;
+		i++;
+	}
+	if (i == index)
+		return (head);
+}
