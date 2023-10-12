6b702984c8c2052bf2c707139754dcb151962e71
Ramyrashad127 <ramy41599@gmail.com>
2023-10-12 10:49:39 -0700

    new

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *test= *head;
+	dlistint_t *test = *head;
 
 	if (!new)
 		return (NULL);
