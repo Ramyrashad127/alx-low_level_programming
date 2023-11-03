e2b28444222af778979d5e754500c76fa289f504
Ramyrashad127 <ramy41599@gmail.com>
2023-11-03 06:51:17 -0700

    new

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include"hash_tables.h"
+/**
+ * hash_table_get- get
+ * @ht: ht
+ * @key: key
+ * Return: st
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int ind;
+	hash_node_t *test;
+
+	if (!ht || !key)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	test = ht->array[index];
+	while(test)
+	{
+		if (strcmp(test->key, key) == 0)
+		{
+			return (test->value);
+		}
+		test = test->next;
+	}
+	return (NULL);
+}
