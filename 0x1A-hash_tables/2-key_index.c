a6f17201e2c051de7652ef5d1089b8c8e648976d
Ramyrashad127 <ramy41599@gmail.com>
2023-11-03 04:13:17 -0700

    new

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,11 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = hash_djb2(key);
+	unsigned long int hash;
 
-	return (hash % size);
+	if (key)
+	{
+		hash = hash_djb2(key);
+		return (hash % size);
+	}
 }
