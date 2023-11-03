3162f1d120fff781f410dd13d1e1822c9804ef84
Ramyrashad127 <ramy41599@gmail.com>
2023-11-03 07:04:23 -0700

    new

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *test;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		test = ht->array[i];
+		while (test)
+		{
+			printf("'%s' : '%s'",test->key, test->value);
+			if ((i != ht->size - 1) && (test->next != NULL))
+				printf(", ");
+			test = test->next;
+		}
+	}
+	printf("}");
+}
