0959642b786b3f33cde250331fefa1ca67deee5e
Ramyrashad127 <ramy41599@gmail.com>
2023-11-03 02:54:50 -0700

    new

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * @size: size of array
  * Return: hash table
  */
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 	unsigned int i = 0;
