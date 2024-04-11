7dbfb76bf738ef6574489ec0fd2c1c6e0535966b
Ramyrashad127 <ramy41599@gmail.com>
2024-04-11 15:00:13 -0700

    new

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+#include <stdio.h>
+/**
+ * binary_search - two pointer
+ * @array: pointer
+ * @size: length
+ * @value: find
+ * Return: index
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int st = 0, en = size - 1, m, j;
+
+	if (array == NULL)
+		return (-1);
+	while (st <= en)
+	{
+		m = (st + en) / 2;
+		if (array[m] == value)
+			return m;
+		else if (array[m] < value)
+			st = m + 1;
+		else
+			en = m - 1;
+		printf("Searching in array: ");
+		for (j = st; j <= en; j++)
+		{
+			printf("%d", array[j]);
+			if (j != en)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	return (-1);
+}
